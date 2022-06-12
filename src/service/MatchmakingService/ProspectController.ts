b09722ccf4598e2fad579fc4348f41caaa2c5b15
Andreas Tarakanov <andy_tarakanov@web.de>
2022-06-12 20:17:46 +0200

    FLOW-21561: prepare new endpoint for update and create searchProfiles

--- src/service/MatchmakingService/ProspectController.ts
+++ src/service/MatchmakingService/ProspectController.ts
@@ -11,15 +11,34 @@ export default class ProspectController extends APIClient {
      * @param token
      */
     async fetchProspect(token: string) {
-        return await this.invokeApiWithErrorHandling<MatchmakingTypes.Prospect>(
-            `/public/search-profiles`,
-            'GET',
-            '',
-            {
-                headers: {
-                    token,
-                },
-            }
-        );
+        return await this.invokeApiWithErrorHandling<MatchmakingTypes.Prospect>(`/public/search-profiles`, 'GET', '', {
+            headers: {
+                token,
+            },
+        });
+    }
+
+    /**
+     * Fetch prospect for a given token
+     * @param token
+     */
+    async createSearchProfile(token: string, searchProfile: MatchmakingTypes.SearchProfile) {
+        return await this.invokeApiWithErrorHandling<MatchmakingTypes.Prospect>(`/public/search-profiles`, 'POST', searchProfile, {
+            headers: {
+                token,
+            },
+        });
+    }
+
+    /**
+     * Fetch prospect for a given token
+     * @param token
+     */
+    async updateSearchProfile(token: string, searchProfile: MatchmakingTypes.SearchProfile) {
+        return await this.invokeApiWithErrorHandling<MatchmakingTypes.Prospect>(`/public/search-profiles/${searchProfile.id}`, 'PUT', searchProfile, {
+            headers: {
+                token,
+            },
+        });
     }
 }
