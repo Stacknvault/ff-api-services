2e9423de9bb6ed78d3178f04d5be433e349aab38
ffoliversaternus <oliver.saternus.@flowfact.de>
2022-06-22 14:49:22 +0200

    FLOW-2257: fix survery controller return type

--- src/service/SurveyService/SurveyController.ts
+++ src/service/SurveyService/SurveyController.ts
@@ -7,10 +7,10 @@ export class SurveyController extends APIClient {
     }
 
     submitFeedbackSurvey = async (survey: FeedbackSurvey) => {
-        return await this.invokeApiWithErrorHandling<FeedbackSurveyDisplayStatus>('/answer', 'POST', survey);
+        return await this.invokeApiWithErrorHandling('/answer', 'POST', survey);
     };
 
     getSurveyDisplayInfo = async () => {
-        return await this.invokeApiWithErrorHandling('/customerAccess', 'GET');
+        return await this.invokeApiWithErrorHandling<FeedbackSurveyDisplayStatus>('/customerAccess', 'GET');
     };
 }
